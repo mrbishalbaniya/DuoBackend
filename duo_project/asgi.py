869b6d84bddf6ef8e61bf1268f3204f90cc4b585
Bishal Baniya <mr.bishal.baniya@gmail.com>
2026-06-29 00:52:13 +0545

    Fix ASGI startup order so Django settings load before chat routing

--- duo_project/asgi.py
+++ duo_project/asgi.py
@@ -9,17 +9,20 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "duo_project.settings")
+
+from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 
 import chat.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "duo_project.settings")
-
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AuthMiddlewareStack(
             URLRouter(chat.routing.websocket_urlpatterns)
         ),
